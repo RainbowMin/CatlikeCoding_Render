9db09583903ff4eb67f63141ad4240bde3395b13
RainbowMin <123438952@qq.com>
2020-06-16 23:40:35 +0800

    模拟正交投影和透视投影

--- Assets/01 Matrices/Me/CameraTransformation.cs
+++ Assets/01 Matrices/Me/CameraTransformation.cs
@@ -6,15 +6,48 @@ namespace Me
 {
     public class CameraTransformation : TransformationBase
     {
+        public enum CameraType
+        {           
+            None,
+
+            [Header("正交投影")]
+            Orthographic,
+
+            [Header("透视投影")]
+            perspective,
+        }
+
+        public CameraType m_CameraType;
+
+        [Range(0,10)]
+        public float _焦距;
+
         public override Matrix4x4 Matrix
         {
             get
             {
                 Matrix4x4 matrix = new Matrix4x4();
-                matrix.SetRow(0, new Vector4(1f,0f,0f,0));
-                matrix.SetRow(1, new Vector4(0f,1f,0f,0));
-                matrix.SetRow(2, new Vector4(0f,0f,0f,0));
-                matrix.SetRow(3, new Vector4(0f,0f,0f,1f));
+
+                if(m_CameraType == CameraType.Orthographic)
+                {
+                    matrix.SetRow(0, new Vector4(1f,0f,0f,0));
+                    matrix.SetRow(1, new Vector4(0f,1f,0f,0));
+                    matrix.SetRow(2, new Vector4(0f,0f,0f,0));
+                    matrix.SetRow(3, new Vector4(0f,0f,0f,1f));
+                }
+                else if(m_CameraType == CameraType.perspective)
+                {
+                    matrix.SetRow(0, new Vector4(_焦距,0f,0f,0));
+                    matrix.SetRow(1, new Vector4(0f,_焦距,0f,0));   
+                    matrix.SetRow(2, new Vector4(0f,0f,0f,0));
+                    matrix.SetRow(3, new Vector4(0f,0f,1f,0));
+                }
+                else 
+                {
+                    matrix = Matrix4x4.identity;
+                }
+
+
                 return matrix;
             }
         }
