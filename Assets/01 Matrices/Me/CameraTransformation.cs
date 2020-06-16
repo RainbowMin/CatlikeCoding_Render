4ab12fc9a194d7f1a84a566c3d4851049d776232
RainbowMin <123438952@qq.com>
2020-06-16 23:18:58 +0800

    正交摄像机

---
+++ Assets/01 Matrices/Me/CameraTransformation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Me
+{
+    public class CameraTransformation : TransformationBase
+    {
+        public override Matrix4x4 Matrix
+        {
+            get
+            {
+                Matrix4x4 matrix = new Matrix4x4();
+                matrix.SetRow(0, new Vector4(1f,0f,0f,0));
+                matrix.SetRow(1, new Vector4(0f,1f,0f,0));
+                matrix.SetRow(2, new Vector4(0f,0f,0f,0));
+                matrix.SetRow(3, new Vector4(0f,0f,0f,1f));
+                return matrix;
+            }
+        }
+    }
+
+}
